0c41e230f6919a917cd3897ef5f21904973162ec
Lee Pavelich <leepavelich@gmail.com>
2021-11-19 22:22:37 +0000

    Added a draw chart title function drawTitle

--- js/bar-chart-library.js
+++ js/bar-chart-library.js
@@ -5,7 +5,10 @@ const drawBarChart = (data, options, element) => {
   const color = 'color' in options ? options.color : 'DeepSkyBlue'
   const spacing = 'spacing' in options ? options.spacing : 15
   const xLabel = 'xLabel' in options ? options.xLabel : 'x'
-  const yLabel = 'yLabel' in options ? options.yLabel: 'y'
+  const yLabel = 'yLabel' in options ? options.yLabel : 'y'
+  const title  = 'title' in options ? options.title : 'Title'
+  const titleColor = 'titleColor' in options ? options.titleColor : 'Black'
+  const titleSize = 'titleSize' in options ? options.titleSize : 24
 
   const offsetX = 100
   const offsetY = 400
@@ -22,7 +25,7 @@ const drawBarChart = (data, options, element) => {
   }
 
   drawAxes([offsetX, offsetY], x, y, xLabel, yLabel, element)
-
+  drawTitle([offsetX, offsetY], x, y, title, titleColor, titleSize, element)
 }
 
 const drawBox = (offsetX, offsetY, x, y, element, color, outline=false) => {
@@ -42,6 +45,7 @@ const drawAxes = (origin, width, height, xLabel, yLabel, element) => {
 
   ctx.font = '20px Arial'
   ctx.fillStyle = 'black'
+
   ctx.textAlign = 'center'
   ctx.fillText(xLabel, origin[0] + width/2, origin[1] + 40)
   ctx.textAlign = 'end'
@@ -54,4 +58,14 @@ const drawAxes = (origin, width, height, xLabel, yLabel, element) => {
   ctx.moveTo(origin[0], origin[1]);
   ctx.lineTo(origin[0] + width, origin[1])
   ctx.stroke()
+}
+
+const drawTitle = (origin, width, height, title, titleColor, titleSize, element) => {
+  let ctx= document.getElementById(element).getContext("2d")
+
+  ctx.font = `${titleSize}px Arial`
+  ctx.fillStyle = titleColor
+
+  ctx.textAlign = 'center'
+  ctx.fillText(title, origin[0] + width/2, origin[1] - height + 40)
 }
